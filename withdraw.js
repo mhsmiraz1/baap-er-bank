16e8dd6f5c888c0b3343bbb4b78b79bffc3152d3
MhsMiraz <mhsmiraz@gmail.com>
2022-12-05 19:41:18 +0600

    added alaert

--- withdraw.js
+++ withdraw.js
@@ -7,19 +7,24 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
    const withdrawTotalElement = document.getElementById('withdraw-total');
    const previousWithdrawTotalString = withdrawTotalElement.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+   const balanceTotalElement = document.getElementById('balance-total');
+   const previousBalanceTotalString = balanceTotalElement.innerText;
+   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+   withdrawField.value = '';
    
+   if(newWithdrawAmmount > previousBalanceTotal){
+      alert('baap er bank ato taka nai!')
+      return;
+   }
    // step 4:
    const currentWithDrawTotal = previousWithdrawTotal + newWithdrawAmmount;
    withdrawTotalElement.innerText = currentWithDrawTotal;
-
-   const balanceTotalElement = document.getElementById('balance-total');
-   const previousBalanceTotalString =balanceTotalElement.innerText;
-   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
    // culculate current balance total
    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmmount;
 
    balanceTotalElement.innerText = currentBalanceTotal;
-
-   withdrawField.value = '';
+   
 })
